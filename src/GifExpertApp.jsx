afbb131f4e6ffc7b0c50cd885a4e7f42e49cea07
JQuintero Web <102717524+JquinteroWeb@users.noreply.github.com>
2023-01-06 07:45:14 -0500

    Styles.css: Added.

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,13 @@
 export const GifExpertApp = () => {
     return (
         <>
+            {/* Title */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* List */}
+
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GifExpertApp } from './GifExpertApp';
-
+//Global styles
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
