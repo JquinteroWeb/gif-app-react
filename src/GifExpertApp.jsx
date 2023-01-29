d64f07c9df48e7b05e9892cc43882cd9f08e3220
JQuinteroWeb <juanpiiktero09@gmail.com>
2023-01-29 11:14:55 -0600

    Add category: modified

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,30 +1,32 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid';
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState([]);
-    const onAddCategory = (newCategory) => {        
-        setCategories(cat => [...cat, newCategory]);
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+        setCategories(cat => [newCategory, ...cat]);
     }
 
     return (
         <>
-            {/* Title */}
+
             <h1>GifExpertApp</h1>
 
-            {/* Input */}
-            <AddCategory
-                onNewCategory = {onAddCategory}
-            />
-            {/* List */}
 
-            <ol>
-                {
-                    categories.map((category) => {
-                        return <li key={category} >{category}</li>
-                    })
-                }
-            </ol>
+            <AddCategory
+                onNewCategory={onAddCategory}
+            />
+
+
+            {
+                categories.map(category => <GifGrid key={category} category={category} />)
+
+            }
+
+
+
 
         </>
     )
